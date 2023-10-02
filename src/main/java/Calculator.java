df01de0994a84a196062f88a9a13f5cbfc2ed86f
Ilya <ilyamironenko1314@gmail.com>
2023-10-02 22:49:17 +0300

    Вторая проба 70%

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,36 +2,32 @@ import java.util.Scanner;
 import java.text.DecimalFormat;
 public class Calculator {
     public static void main(String[] args) {
+        }
 
+
+    public static void calculation() {
         String soglasie;
         String calcul = "Ваши товары: ";
         Float sum = 0.00f;
-
         Scanner scan = new Scanner(System.in);
-        //System.out.println("Напишите название товара:");
-        //String tovar = scan.next();
 
-        //System.out.println("Напишите стоимость товара (руб.коп.):");
-        //float cena = scan.nextFloat();
-
-
-          while (true){
+        while (true){
             System.out.println("Напишите название товара:");
             String tovar = scan.next();
 
-            calcul = calcul + " " + tovar;
+
 
             System.out.println("Напишите стоимость товара (руб.коп.):");
             float cena = scan.nextFloat();
             sum = sum + cena;
 
-
+            calcul = calcul + " " + tovar + cena;
+            System.out.println("На данный момент Ваши товары: "+ calcul);
             System.out.println("Хотите заказать ещё? тогда напшите любой символ. Напишите: Завершить, если всё заказали");
             soglasie = scan.next();
             if (soglasie.equals("Завершить")){
                 break;
             }
-        }
     }
-}
+}}
 
